83fbabf19bfa2b0336152f4d7b6c299f7e3a3f02
Nixyz11 <nikola.bogdanovic.ssfon@gmail.com>
2022-12-29 19:34:51 +0100

    obrisan src, postavljenje dependenceis u package-u

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
-import logo from './logo.svg';
 import './App.css';
+import {Routes, Route, Link, Navigate} from 'react-router-dom'
+import {Layout,Typography,Space} from 'antd'
+import {Navbar, Exchanges,Homepage,Cryptocurrencies,News,Cryptodetails } from './components';
+
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React-
-        </a>
-      </header>
+    <div className='app'>
+      <div className='navbar'>
+        <Navbar/>
+      </div>
+      <div className='main'>
+        <Layout>
+          <div className='routes'>
+            <Routes>
+              <Route path='/home' element={<Homepage />}>
+                
+              </Route>
+              <Route path='/exchanges' element={<Exchanges />}>
+               
+              </Route>
+              <Route path='/cryptocurrencies' element={<Cryptocurrencies simplified={false}/>}>
+                
+              </Route>
+              <Route path='/crypto/:coinId' element={<Cryptodetails />}> 
+               
+              </Route>
+              <Route path='/news' element={ <News simplified={false}/>}>
+               
+              </Route>
+              <Route
+                path="/"
+                  element={<Navigate to="/home" replace />}
+                          />
+            </Routes>
+          </div>
+        </Layout>
+    
+      <div className='footer' >
+        <Typography.Title level={5} style={{color: 'white',textAlign: 'center'}}>
+          Cryptoverse <br/>
+          All rights reserved
+        </Typography.Title>
+        <Space>
+          <Link to="/home">Home</Link>
+          <Link to="/exchanges">Exchanges</Link>
+          
+          <Link to="/news">News</Link>
+        </Space>
+        </div>
+      </div>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+import 'antd/dist/antd.css'
+import store from './app/store';
+import { Provider } from 'react-redux';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+  <BrowserRouter>
   <React.StrictMode>
+    <Provider store={store}>
     <App />
+    </Provider>
   </React.StrictMode>
+  </BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
